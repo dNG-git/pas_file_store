82f78b075fffd598b17ecf37fb6d5584822dd7c0
NotTheEvilOne <git-NTEO@vplace.de>
2015-11-09 11:49:47 +0100

    Trivial: Fixed references to the "pas.core" module

--- src/dNG/pas/vfs/x_file_store/object.py
+++ src/dNG/pas/vfs/x_file_store/object.py
@@ -6,7 +6,7 @@ direct PAS
 Python Application Services
 ----------------------------------------------------------------------------
 (C) direct Netware Group - All rights reserved
-https://www.direct-netware.de/redirect?pas;core
+https://www.direct-netware.de/redirect?pas;file_store
 
 This Source Code Form is subject to the terms of the Mozilla Public License,
 v. 2.0. If a copy of the MPL was not distributed with this file, You can
@@ -14,7 +14,7 @@ obtain one at http://mozilla.org/MPL/2.0/.
 ----------------------------------------------------------------------------
 https://www.direct-netware.de/redirect?licenses;mpl2
 ----------------------------------------------------------------------------
-#echo(pasCoreVersion)#
+#echo(pasFileStoreVersion)#
 #echo(__FILEPATH__)#
 """
 
