7181b00035a7429aa1fc737aee132fc79491ca5b
NotTheEvilOne <git-NTEO@vplace.de>
2016-08-10 10:16:14 +0200

    Enhancement: Added implementation to store a cached copy of a VFS file object transfered in the background

---
+++ src/dNG/plugins/tasks/pas_file_store.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+##j## BOF
+
+"""
+direct PAS
+Python Application Services
+----------------------------------------------------------------------------
+(C) direct Netware Group - All rights reserved
+https://www.direct-netware.de/redirect?pas;file_store
+
+This Source Code Form is subject to the terms of the Mozilla Public License,
+v. 2.0. If a copy of the MPL was not distributed with this file, You can
+obtain one at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------------------------
+https://www.direct-netware.de/redirect?licenses;mpl2
+----------------------------------------------------------------------------
+#echo(pasFileStoreVersion)#
+#echo(__FILEPATH__)#
+"""
+
+# pylint: disable=unused-argument
+
+from dNG.vfs.cached_implementation import CachedImplementation
+from dNG.plugins.hook import Hook
+from dNG.runtime.value_exception import ValueException
+
+def store_vfs_url(params, last_return = None):
+#
+	"""
+Called for "dNG.pas.vfs.CachedObject.storeVfsUrl"
+
+:param params: Parameter specified
+:param last_return: The return value from the last hook called.
+
+:since: v0.2.00
+	"""
+
+	if (last_return is not None): _return = last_return
+	elif ("vfs_url" not in params): raise ValueException("Missing required argument")
+	else:
+	#
+		CachedImplementation.store_cached_vfs_url(params['vfs_url'], params.get("cached_data", { }))
+		_return = True
+	#
+
+	return _return
+#
+
+def register_plugin():
+#
+	"""
+Register plugin hooks.
+
+:since: v0.2.00
+	"""
+
+	Hook.register("dNG.pas.vfs.CachedObject.storeVfsUrl", store_vfs_url)
+#
+
+def unregister_plugin():
+#
+	"""
+Unregister plugin hooks.
+
+:since: v0.2.00
+	"""
+
+	Hook.unregister("dNG.pas.vfs.CachedObject.storeVfsUrl", store_vfs_url)
+#
+
+##j## EOF
---
+++ src/dNG/vfs/cached_implementation.py
@@ -0,0 +1,195 @@
+# -*- coding: utf-8 -*-
+##j## BOF
+
+"""
+direct PAS
+Python Application Services
+----------------------------------------------------------------------------
+(C) direct Netware Group - All rights reserved
+https://www.direct-netware.de/redirect?pas;file_store
+
+This Source Code Form is subject to the terms of the Mozilla Public License,
+v. 2.0. If a copy of the MPL was not distributed with this file, You can
+obtain one at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------------------------
+https://www.direct-netware.de/redirect?licenses;mpl2
+----------------------------------------------------------------------------
+#echo(pasFileStoreVersion)#
+#echo(__FILEPATH__)#
+"""
+
+from dNG.data.cache.file import File
+from dNG.data.logging.log_line import LogLine
+from dNG.database.connection import Connection
+from dNG.database.nothing_matched_exception import NothingMatchedException
+from dNG.runtime.io_exception import IOException
+
+try:
+#
+	from dNG.data.tasks.database_proxy import DatabaseProxy as DatabaseTasks
+	from dNG.tasks.database_lrt_hook import DatabaseLrtHook
+#
+except ImportError: DatabaseTasks = None
+
+from .implementation import Implementation
+
+class CachedImplementation(Implementation):
+#
+	"""
+"CachedImplementation" provides implementation independent methods to access
+VFS objects that might be cached locally.
+
+:author:     direct Netware Group et al.
+:copyright:  direct Netware Group - All rights reserved
+:package:    pas
+:subpackage: file_store
+:since:      v0.2.00
+:license:    https://www.direct-netware.de/redirect?licenses;mpl2
+             Mozilla Public License, v. 2.0
+	"""
+
+	@staticmethod
+	def cache_vfs_url(vfs_url, cached_data = None):
+	#
+		"""
+Stores a given VFS URL in cache using the given cache data. It will be
+scheduled as a background task.
+
+:param vfs_url: VFS URL
+:param cached_data: Cache data for the VFS URL
+
+:since: v0.2.00
+		"""
+
+		if (DatabaseTasks is None
+		    or (not DatabaseTasks.is_available())
+		   ): LogLine.warning("pas.file_store vfs.CachedImplementation requested to cache VFS URL '{0}' but 'pas.tasks' is not available", vfs_url, context = "pas_file_store")
+		else:
+		#
+			database_tasks = DatabaseTasks.get_instance()
+
+			database_task_data = { "vfs_url": vfs_url }
+
+			if (cached_data is not None
+			    and len(database_task_data) > 0
+			   ): database_task_data['cached_data'] = cached_data
+
+			database_tasks.add("dNG.pas.vfs.CachedObject.storeVfsUrl.{0}".format(vfs_url),
+			                   DatabaseLrtHook("dNG.pas.vfs.CachedObject.storeVfsUrl",
+			                                   **database_task_data
+			                                  ),
+			                   1
+			                  )
+		#
+	#
+
+	@Connection.wrap_callable
+	@staticmethod
+	def cache_vfs_url_if_changed(vfs_url, cached_data = None):
+	#
+		"""
+Stores a given VFS URL in cache using the given cache data. It will be
+scheduled as a background task.
+
+:param vfs_url: VFS URL
+:param cached_data: Cache data for the VFS URL
+
+:since: v0.2.00
+		"""
+
+		cached_file = None
+		is_cached_vfs_url_up_to_date = False
+		vfs_object = None
+
+		try:
+		#
+			vfs_object = Implementation.load_vfs_url(vfs_url, True)
+
+			try:
+			#
+				cached_file = File.load_resource(vfs_url)
+
+				vfs_object_time_updated = (vfs_object.get_time_updated()
+				                           if (vfs_object.is_supported("time_updated")) else
+				                           None
+				                          )
+
+				is_cached_vfs_url_up_to_date = (cached_file.is_valid()
+				                                and (vfs_object_time_updated is None
+				                                     or cached_file.is_up_to_date(vfs_object_time_updated)
+				                                    )
+				                               )
+			#
+			except NothingMatchedException: pass
+		#
+		finally:
+		#
+			if (cached_file is not None): cached_file.close()
+			if (vfs_object is not None): vfs_object.close()
+		#
+
+		if (not is_cached_vfs_url_up_to_date):
+		#
+			CachedImplementation.cache_vfs_url(vfs_url, cached_data)
+		#
+	#
+
+	@staticmethod
+	def load_vfs_url(vfs_url, readonly = False):
+	#
+		"""
+Returns the initialized object instance for the given VFS URL.
+
+:param vfs_url: VFS URL
+:param readonly: Open object in readonly mode
+
+:return: (object) VFS object instance
+:since:  v0.2.00
+		"""
+
+		if (readonly):
+		#
+			try:
+			#
+				cached_file = File.load_resource(vfs_url)
+				vfs_url = cached_file.get_vfs_url()
+			#
+			except NothingMatchedException: pass
+		#
+
+		return Implementation.load_vfs_url(vfs_url, readonly)
+	#
+
+	@staticmethod
+	def store_cached_vfs_url(vfs_url, cached_data = None):
+	#
+		"""
+Stores a given VFS URL in cache using the given cache data.
+
+:param vfs_url: VFS URL
+:param cached_data: Cache data for the VFS URL
+
+:since: v0.2.00
+		"""
+
+		if (cached_data is None): cached_data = { }
+
+		vfs_object = Implementation.load_vfs_url(vfs_url, True)
+
+		try:
+		#
+			if (not vfs_object.is_file()): raise IOException("VFS URL given is not a cacheable file")
+
+			cached_file = File()
+			vfs_object.copy_data(cached_file)
+		#
+		finally: vfs_object.close()
+
+		cached_data['resource'] = vfs_url
+
+		cached_file.set_data_attributes(**cached_data)
+		cached_file.save()
+	#
+#
+
+##j## EOF
